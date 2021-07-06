374ea4611a0c93cc111977229918b026e9c0dc11
Benício Barreto <benicio.daniel@gmail.com>
2021-07-06 18:17:13 -0300

    creating user list page mockup

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Text,
+  Td,
+  Th,
+  Thead,
+  Tr
+} from '@chakra-ui/react';
+import { Header } from '../../components/Header';
+import { Sidebar } from '../../components/Sidebar';
+import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" maxWidth={1480} my="6" mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bgColor="gray.800" p="8">
+          <Flex mb="8" justifyContent="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" w="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+                <Th></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Benício Daniel</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      benicio@uol.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>05 de julho, 2021</Td>
+                <Td textAlign="right">
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Benício Daniel</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      benicio@uol.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>05 de julho, 2021</Td>
+                <Td textAlign="right">
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
