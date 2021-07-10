540e1f1118b49dfa7917a641194adcfbca8207d6
Benício Barreto <benicio.daniel@gmail.com>
2021-07-10 20:03:49 -0300

    added react query to cache data

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -13,26 +13,27 @@ import {
   Th,
   Thead,
   Tr,
-  useBreakpointValue
+  useBreakpointValue,
+  Spinner
 } from '@chakra-ui/react';
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Pagination } from '../../components/Pagination';
 import Link from 'next/link';
-import { useEffect } from 'react';
+import { useQuery } from 'react-query';
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users');
+    const data = response.json();
+    return data;
+  });
+
   const isWiderVersion = useBreakpointValue({
     base: false,
     lg: true
   });
-
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  }, []);
 
   return (
     <Box>
@@ -58,72 +59,84 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha" overflowX="scroll">
-            <Thead>
-              <Tr>
-                <Th px={['4', '4', '6']} color="gray.300" w="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                {isWiderVersion && <Th>Data de cadastro</Th>}
-                <Th></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Benício Daniel</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      benicio@uol.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWiderVersion && <Td>05 de julho, 2021</Td>}
-                <Td textAlign="right">
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    {isWiderVersion ? 'Editar' : ''}
-                  </Button>
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Benício Daniel</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      benicio@uol.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWiderVersion && <Td>05 de julho, 2021</Td>}
-                <Td textAlign="right">
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    {isWiderVersion ? 'Editar' : ''}
-                  </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados dos usuários</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha" overflowX="scroll">
+                <Thead>
+                  <Tr>
+                    <Th px={['4', '4', '6']} color="gray.300" w="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWiderVersion && <Th>Data de cadastro</Th>}
+                    <Th></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td px={['4', '4', '6']}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Benício Daniel</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          benicio@uol.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWiderVersion && <Td>05 de julho, 2021</Td>}
+                    <Td textAlign="right">
+                      <Button
+                        as="a"
+                        size="sm"
+                        fontSize="sm"
+                        colorScheme="purple"
+                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                      >
+                        {isWiderVersion ? 'Editar' : ''}
+                      </Button>
+                    </Td>
+                  </Tr>
+                  <Tr>
+                    <Td px={['4', '4', '6']}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Benício Daniel</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          benicio@uol.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWiderVersion && <Td>05 de julho, 2021</Td>}
+                    <Td textAlign="right">
+                      <Button
+                        as="a"
+                        size="sm"
+                        fontSize="sm"
+                        colorScheme="purple"
+                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                      >
+                        {isWiderVersion ? 'Editar' : ''}
+                      </Button>
+                    </Td>
+                  </Tr>
+                </Tbody>
+              </Table>
 
-          <Pagination />
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
