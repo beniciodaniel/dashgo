eccbc5780af6f3ed02c6a01b20bc1f894cebe998
Benício Barreto <benicio.daniel@gmail.com>
2021-07-11 23:16:08 -0300

    added pagination logic to mirage config

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model } from 'miragejs';
+import { createServer, Factory, Model, Response } from 'miragejs';
 import faker from 'faker';
 
 type User = {
@@ -22,7 +22,7 @@ export function makeServer() {
           return faker.internet.email().toLowerCase();
         },
         createdAt() {
-          return faker.date.recent(10);
+          return faker.date.recent(200);
         }
       })
     },
@@ -35,7 +35,29 @@ export function makeServer() {
       this.namespace = 'api';
       this.timing = 750;
 
-      this.get('/users');
+      this.get('/users', function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams;
+
+        const total = schema.all('user').length;
+
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = this.serialize(schema.all('user')).users.slice(
+          pageStart,
+          pageEnd
+        );
+
+        return new Response(
+          200,
+          {
+            'x-total-count': String(total)
+          },
+          {
+            users
+          }
+        );
+      });
       this.post('/users');
 
       this.namespace = '';
